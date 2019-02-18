a09eff8f11c071b6246eb8f5692af3efea3447ab
Joel L <joelmleifer@gmail.com>
2019-02-18 23:27:40 +1100

    fixed dimensions and ratios
    added correct ratios of weight and mass to solar system, fixed dimensions to work on vertical screens.

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,34 @@
+var x = window.innerWidth - 2;
+var y = x;
+if (window.innerHeight < window.innerWidth) {
+    y = window.innerHeight - 2;
+    x = y;
+}
+const rRatio = (2*y/5)/(5.906292*10**12)
+const mRatio = (1.989*10**8.5)/(1.989*10**30);
 var celestialObjects = [
     {
         name:'Sun',
         pos: {
-            x: window.innerHeight/2,//Math.floor(Math.random() * (window.innerHeight - 1)), // accounting for canvas dimensions (-2 + 1)
-            y: window.innerHeight/2//Math.floor(Math.random() * (window.innerHeight - 1))
+            x: x/2,
+            y: y/2
         },
-        vel: {  // m/s
+        vel: {  // px/s
             x: 0,
             y: 0
         },
-        acc: {  // m/s/s
+        acc: {  // px/s/s
             x: 0,
             y: 0
         },
-        mass: 1.989 * Math.pow(10,8.5),  // Kg
-        radius: 20  // m
+        mass: 1.989*10**8.5,
+        radius: y/30 // px
     },
     {
         name:'Mercury',
         pos: {
-            x: window.innerHeight/2,//Math.floor(Math.random() * (window.innerHeight - 1)),
-            y:285//Math.floor(Math.random() * (window.innerHeight - 1))
+            x: x/2,
+            y: (9*y/20) -  rRatio*57.91*10**9
         },
         vel: {
             x: 0,
@@ -30,14 +38,14 @@ var celestialObjects = [
             x: 0,
             y: 0
         },
-        mass: 0.0000001,
+        mass: mRatio*3.301*10**23,
         radius: 1
     },
     {
         name:'Venus',
         pos: {
-            x: window.innerHeight/2,//Math.floor(Math.random() * (window.innerHeight - 1)),
-            y:270//Math.floor(Math.random() * (window.innerHeight - 1))
+            x: x/2,
+            y: (9*y/20) -  rRatio*108.2*10**9
         },
         vel: {
             x: 0,
@@ -47,14 +55,14 @@ var celestialObjects = [
             x: 0,
             y: 0
         },
-        mass: 0.0000001,
-        radius: 3
+        mass: mRatio*4.867*10**24,
+        radius: 1.5
     },
     {
         name:'Earth',
         pos: {
-            x: window.innerHeight/2,//Math.floor(Math.random() * (window.innerHeight - 1)),
-            y:240//Math.floor(Math.random() * (window.innerHeight - 1))
+            x: x/2,
+            y: (9*y/20) -  rRatio*149.6*10**9
         },
         vel: {
             x: 0,
@@ -64,14 +72,14 @@ var celestialObjects = [
             x: 0,
             y: 0
         },
-        mass: 0.0000001,
-        radius: 4
+        mass: mRatio*5.972*10**24,
+        radius: 2
     },
     {
         name:'Mars',
         pos: {
-            x: window.innerHeight/2,//Math.floor(Math.random() * (window.innerHeight - 1)),
-            y: 200//Math.floor(Math.random() * (window.innerHeight - 1))
+            x: x/2,
+            y: (9*y/20) -  rRatio*227.9*10**9
         },
         vel: {
             x: 0,
@@ -81,14 +89,14 @@ var celestialObjects = [
             x: 0,
             y: 0
         },
-        mass: 0.0000001,
-        radius: 1
+        mass: mRatio*6.4127*10**23,
+        radius: 1.5
     },
     {
         name:'Jupiter',
         pos: {
-            x: window.innerHeight/2,//Math.floor(Math.random() * (window.innerHeight - 1)),
-            y: 170//Math.floor(Math.random() * (window.innerHeight - 1))
+            x: x/2,
+            y: (9*y/20) -  rRatio*778.5*10**9
         },
         vel: {
             x: 0,
@@ -98,14 +106,14 @@ var celestialObjects = [
             x: 0,
             y: 0
         },
-        mass: 0.0000001,
-        radius: 7
+        mass: mRatio*1.899*10**27,
+        radius: 3
     },
     {
         name:'Saturn',
         pos: {
-            x: window.innerHeight/2,//Math.floor(Math.random() * (window.innerHeight - 1)),
-            y: 140//Math.floor(Math.random() * (window.innerHeight - 1))
+            x: x/2,
+            y: (9*y/20) -  rRatio*1.434*10**12
         },
         vel: {
             x: 0,
@@ -115,14 +123,14 @@ var celestialObjects = [
             x: 0,
             y: 0
         },
-        mass: 0.0000001,
-        radius: 6
+        mass: mRatio*5.685*10**26,
+        radius: 2.5
     },
     {
         name:'Uranus',
         pos: {
-            x: window.innerHeight/2,//Math.floor(Math.random() * (window.innerHeight - 1)),
-            y: 120//Math.floor(Math.random() * (window.innerHeight - 1))
+            x: x/2,
+            y: (9*y/20) -  rRatio*2.871*10**12
         },
         vel: {
             x: 0,
@@ -132,14 +140,31 @@ var celestialObjects = [
             x: 0,
             y: 0
         },
-        mass: 0.0000001,
-        radius: 5
+        mass: mRatio*8.682*10**25,
+        radius: 2
+    },
+    {
+        name:'Neptune',
+        pos: {
+            x: x/2,
+            y: (9*y/20) -  rRatio*4.495*10**12
+        },
+        vel: {
+            x: 0,
+            y: 0
+        },
+        acc: {
+            x: 0,
+            y: 0
+        },
+        mass: mRatio*1.024*10**26,
+        radius: 2
     },
     {
         name:'Pluto',
         pos: {
-            x: window.innerHeight/2,//Math.floor(Math.random() * (window.innerHeight - 1)),
-            y: 100//Math.floor(Math.random() * (window.innerHeight - 1))
+            x: x/2,
+            y: (9*y/20) - 2*y/5
         },
         vel: {
             x: 0,
@@ -149,8 +174,8 @@ var celestialObjects = [
             x: 0,
             y: 0
         },
-        mass: 0.0000001,
-        radius: 1
+        mass: mRatio*1.471*10**22,
+        radius: 0.5
     }
 ]
 
@@ -173,6 +198,7 @@ var update = function() {
 
 var updating;
 var start = function(tickrate) {
+    clearInterval(updating)
     for (let i = 1; i < celestialObjects.length; i++) {
         celestialObjects[i].vel.x=Math.sqrt(G*celestialObjects[0].mass/(window.innerHeight/2 - celestialObjects[i].pos.y));
     }
@@ -180,4 +206,8 @@ var start = function(tickrate) {
 }
 var stop = function() {
     clearInterval(updating);
+}
+var resume = function(tickrate) {
+    clearInterval(updating)
+    updating = setInterval(update,1000/tickrate)
 }
