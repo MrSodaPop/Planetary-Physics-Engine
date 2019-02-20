a29c36dcf44e9673a7802ae71a868c5a3524e2ab
MrSodaPop <31635932+MrSodaPop@users.noreply.github.com>
2019-02-20 20:50:44 +1100

    Added basic collisions
    buggy collisions, imperfect, not correct angles, favours whichever velocity is calculated first. based on Momentum (P) calculations

--- JS/calculations.js
+++ JS/calculations.js
@@ -2,6 +2,25 @@
 const G = 0.0000000000667;
 
 var updatePosition = function(object) {
+    for (let i = 0; i < celestialObjects.length; i++) {
+        if (object.name != celestialObjects[i].name) {
+            let r = Math.sqrt(Math.pow((celestialObjects[i].pos.y - object.pos.y),2)+Math.pow((celestialObjects[i].pos.x - object.pos.x),2))
+            if (r <= celestialObjects[i].radius + object.radius + 1) {
+                let PObject = {
+                    x: object.mass * object.vel.x,
+                    y: object.mass * object.vel.y
+                };
+                let PCelestial = {
+                    x: celestialObjects[i].mass * celestialObjects[i].vel.x,
+                    y: celestialObjects[i].mass * celestialObjects[i].vel.y
+                };
+                PObject.x += PCelestial.x;
+                PObject.y += PCelestial.y;
+                object.vel.x = (PObject.x/2)/object.mass;
+                object.vel.y = (PObject.y/2)/object.mass;
+            }
+        }
+    }
     object.pos.x = object.pos.x + object.vel.x;
     object.pos.y = object.pos.y + object.vel.y;
     return object;
@@ -22,16 +41,13 @@ var updateAcceleration = function(object) {
         if (object.name !== celestialObjects[i].name) {
             let r = Math.sqrt(Math.pow((celestialObjects[i].pos.y - object.pos.y),2)+Math.pow((celestialObjects[i].pos.x - object.pos.x),2))    // Finding Distance between the two objects
             let theta = Math.atan((celestialObjects[i].pos.y - object.pos.y)/(celestialObjects[i].pos.x - object.pos.x))    // Using pos of both objects to find agnle of acceleration.
-            let sin = 1;
-            let cos = 1;
+            let inverse = 1;
             if (celestialObjects[i].pos.x < object.pos.x) {
-                    sin = -1;
-                    cos = -1;
+                    inverse = -1
             }
             let totalAcc = G*celestialObjects[i].mass/(Math.pow(r,2));
-
-            newAcceleration.x = newAcceleration.x + Math.cos(theta) * totalAcc * cos;
-            newAcceleration.y = newAcceleration.y + Math.sin(theta) * totalAcc * sin;
+            newAcceleration.x = newAcceleration.x + Math.cos(theta) * totalAcc * inverse;
+            newAcceleration.y = newAcceleration.y + Math.sin(theta) * totalAcc * inverse;
         }
     }
     object.acc = newAcceleration;
